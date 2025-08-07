05e718973e21b3fe12bbad8fa1f2a8d3b87b7b2e
Suesyk <Samuel.syk@aliyun.com>
2025-08-07 16:18:58 +0800

    Site updated: 2025-08-07 16:18:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/example/","2025/08/07/123/","2025/08/07/4321/","2025/08/07/hello-world/"];function toRandomPost(){
+var posts=["2024/03/01/3D/","2025/05/01/achievement/","2024/06/01/SUN/","2022/08/01/example/","2024/10/29/love/","2025/08/07/hello-world/","2025/06/01/cable/","2025/07/07/plane/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
