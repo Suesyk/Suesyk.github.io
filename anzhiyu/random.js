36648215e07cb3c8dc41782c1ace6fcf64d4853f
Suesyk <Samuel.syk@aliyun.com>
2025-08-07 14:58:54 +0800

    Site updated: 2025-08-07 14:58:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/07/123/","2025/08/07/hello-world/","2025/08/07/4321/","2025/08/07/example/"];function toRandomPost(){
+var posts=["2025/08/07/123/","2025/08/07/example/","2025/08/07/hello-world/","2025/08/07/4321/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
